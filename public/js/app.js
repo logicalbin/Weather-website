7aad0ec7fa720242e18a4421da595b82fa3d619e
logicalbin <72993365+logicalbin@users.noreply.github.com>
2021-02-07 20:14:10 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit',(e)=>{
     console.log(location)
     weatherForm.reset()
     message1.textContent='Loading.....'
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             message1.textContent=' '
